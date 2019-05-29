536320fc43b848a4774b30fcd4713daea9c5f12d
Philippe Recto <precto1285@gmal.com>
2019-05-29 13:30:15 -0700

    added table, event-listner

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ Contact = (firstName, lastName, email) => {
   this.firstName = firstName;
   this.lastName = lastName;
   this.email = email;
-}
+};
+
+// UI Constructor
+UI = () => { };
+
+
+// Event listener to add Contact
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+  console.log('clicked');
+  e.preventDefault();
+});
